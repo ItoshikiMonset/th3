778febd6a17fcb62686f64906e82b0016f0f0f91
susutun9 <stun9@mail.ccsf.edu>
2018-04-08 08:51:28 +0630

    Add files via upload

---
+++ hosts/upload/drive.google.com.index.php
@@ -0,0 +1,12 @@
+<?php
+$upload_services[] = 'drive.google.com';
+$max_file_size['drive.google.com'] = false ;
+$page_upload['drive.google.com'] = 'drive.google.com.php';
+
+/**
+ *  Plugin Name: Google Drive Upload Plugin
+ *  Written By: FastRapidLeech.Com
+ *  Date : 07-April-2018
+ */
+
+?>
